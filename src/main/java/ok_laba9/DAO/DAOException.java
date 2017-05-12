2e3c038fe51a6db2271ad0c8c5ed74fd745b038e
Oksana Severovostokova <ksjuha-21@mail.ru>
2017-05-12 14:28:39 +0300

    change1

---
+++ src/main/java/ok_laba9/DAO/DAOException.java
@@ -0,0 +1,7 @@
+package ok_laba9.DAO;
+
+/**
+ * Created by toshiba_admin on 12.05.2017.
+ */
+public class DAOException {
+}
--- src/main/java/ok_laba9/DAO/UserDAO.java
+++ src/main/java/ok_laba9/DAO/UserDAO.java
@@ -1,8 +1,29 @@
 package ok_laba9.DAO;
 
+import ok_laba9.DAO.DAOException
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+
 /**
  * Created by toshiba_admin on 12.05.2017.
  */
-public interface UserDAO {
+public interface UserDAO<K,T> {
+    boolean create(T entity, Role role, Course course) throws DAOException;
+
+
+    boolean deleteFromUser (K id) throws DAOException;
+
+    T update(T entity) throws DAOException;
+
+    T getUserFindInfoForUser(K id, T user) throws DAOException;
+
+    T getIdByLoginAndPassword(String login, String password) throws DAOException;
+
+    List<T> getAllUserByIdCourse (Course course) throws DAOException;
+
+
+
 
 }
+
